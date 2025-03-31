49e15dce46509894a8926eb4caf021b78616afe3
wdthor <wdthor@proton.me>
2025-03-31 15:23:12 +0200

    Remove boilerplate and add persisted counter with localStorage

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,43 +1,83 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
+  <q-page
+    v-touch-pan.vertical.prevent.mouse="handlePan"
+    class="row items-end justify-evenly text-white"
+  >
+    <section class="row full-width items-center">
+      <div class="col text-center">
+        <q-btn
+          @click="decrease"
+          v-touch-repeat:300:300:300:300:50.mouse="decrease"
+          icon="remove"
+          size="xl"
+          round
+        />
+      </div>
+      <div class="col text-center text-h2">{{ count }}</div>
+      <div class="col text-center">
+        <q-btn
+          @click="increase"
+          v-touch-repeat:300:300:300:300:50.mouse="increase"
+          icon="add"
+          size="xl"
+          round
+        />
+      </div>
+    </section>
+    <section class="row q-pb-xl">
+      <q-btn @click="reset" icon="restart_alt" size="xl" round />
+    </section>
   </q-page>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-import type { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
+import { onMounted, ref, watch } from 'vue';
+import type { TouchPanValue } from 'quasar';
+import { useQuasar } from 'quasar';
 
-const todos = ref<Todo[]>([
-  {
-    id: 1,
-    content: 'ct1'
-  },
-  {
-    id: 2,
-    content: 'ct2'
-  },
-  {
-    id: 3,
-    content: 'ct3'
-  },
-  {
-    id: 4,
-    content: 'ct4'
-  },
-  {
-    id: 5,
-    content: 'ct5'
+const $q = useQuasar();
+
+const count = ref(0);
+
+const increase = () => {
+  count.value++;
+};
+
+const decrease = () => {
+  if (count.value > 0) {
+    count.value--;
   }
-]);
+};
 
-const meta = ref<Meta>({
-  totalCount: 1200
+const reset = () => {
+  count.value = 0;
+};
+
+const handlePan: TouchPanValue = (details) => {
+  if (details.delta?.y !== undefined) {
+    if (details.delta.y < 0) {
+      increase();
+    } else {
+      decrease();
+    }
+  }
+};
+
+onMounted(() => {
+  const counter = Number($q.localStorage.getItem('counter'));
+  if (!isNaN(counter)) {
+    count.value = counter;
+  }
+});
+
+watch(count, (newVal) => {
+  $q.localStorage.set('counter', newVal);
 });
 </script>
+
+<style scoped>
+.q-page {
+  max-width: 700px;
+  margin: 0 auto;
+}
+</style>
